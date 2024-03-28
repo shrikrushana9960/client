ef7d89250e03ccb33eb6d79a9790be04a0388272
Shri <papupingale123@gmail.com>
2024-03-29 00:28:26 +0530

    fix

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,21 +1,23 @@
-import axios from 'axios';
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api/v1';
+import axios from "axios";
+const baseURL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1";
 
 const Api = axios.create({
   baseURL,
+  withCredentials: true,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
-  withCredentials: true
+ 
 });
 Api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 500) {
-      console.error('HTTP 500 Internal Server Error:', error);
+      console.error("HTTP 500 Internal Server Error:", error);
 
-      throw new Error('Internal Server Error');
+      throw new Error("Internal Server Error");
     }
     return Promise.reject(error);
   }
